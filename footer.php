9d91a52945609ef95b9314ca892a71db60ebfa1b
nmcox <nmcox@oakland.edu>
2018-01-14 12:01:57 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <!--File Name: footer.php-->
 <!--Date Created: 1-14-2018-->
-<!--Start File-->
 <!--Created By: Nicole Cox-->
+<!--Start File-->
 
  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
