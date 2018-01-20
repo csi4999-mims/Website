62b041e05fb76bf9adf1de2756a661a1b518d46b
tcwatling <35177680+tcwatling@users.noreply.github.com>
2018-01-20 16:12:34 -0500

    Update register.php
    added php queries

--- register.php
+++ register.php
@@ -10,22 +10,31 @@ require('lib.php');
 
 
 <?php
-//Alex & Tyler
-//$servername = "csi4999mims.online";
-$username = "tcwatling";
-$password = "dankmemes";
-//$databasename = "mims";
-try {
-    $conn = new PDO('mysql:host=50.116.24.189;port=3307;dbname=mims', $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    //echo "Connection Failed: " . $e->getCause();
-    }
+
+$site_root = $_SERVER['connection.php'];
+require_once("$site_root");
+
+//"F" stands for "Form" because we are grabbing from a form
+$Email = $_POST['email'];
+$pass = $_POST['pass'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$phone = $_POST['phone'];
+
+//$sql = "INSERT INTO profiles VALUES ( NULL, '$username', '$password', '$email')";
+//$query = $conn->prepare ( $sql );
+
+//This way may be safer
+$sql = "INSERT INTO profiles ( :pEmail, :pPass, :fname, :lname, :pPhone ) VALUES ( Email, pass, firstname, lastname, phone )";
+$query = $conn->prepare( $sql );
+$result = $query->execute( array( ':pEmail'=>$Email, ':pPass'=>$pass, ':fname'=>$firstname, ':lname'=>$lastname, ':pPhone'=>$phone ) );
+
+if ( $result ){
+  echo "<p>Thank you. You have been registered</p>";
+} else {
+  echo "<p>Sorry, there has been a problem inserting your details. Please contact admin.</p>";
+}
+
 ?>
 
 <!DOCTYPE html>
