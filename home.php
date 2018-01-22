1d0a29a7373b885d1e47bf086e9f029e919d68c1
apmorey <30659268+apmorey@users.noreply.github.com>
2018-01-22 13:36:44 -0500

    Update home.php
    Added the query code for registration and add/commented out the authenticate login code until the registration query can be moved.

--- home.php
+++ home.php
@@ -8,6 +8,66 @@
 require('lib.php');
 ?>
 
+<?php
+$sire_root = $_SERVER['DOCUMENT_ROOT'];
+require_once("$site_root/../connection.php");
+
+try {
+    $email = $_POST['email'];
+    $pass = $_POST9'pass'];
+    $phone = $_POST['phone'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    
+    $sql = "INSERT INTO profiles (firstname, lastname, email, pass, phone) VALUES ('" . $firstname . "', '" . $lastname . "', '" . $email . "', '" . $pass . "', '" . $phone . "')";
+    $conn->exec($sql);
+    $conn = null;
+    
+} catch(PDOException $e) {
+    echo $e->getMessage();
+}
+
+?>
+
+<?php
+/*
+
+$site_root = $_SERVER['DOCUMENT_ROOT'];
+require_once("$site_root/../connection.php");
+
+if(isset($_POST['login'])) {
+    $email = !empty($_POST['email']) ? trim($_POST['email']) : null;
+    passwordAttempt = !empty($_POST['pass']) ? trim($_POST['pass']) : null;
+    
+    $sql = "SELECT email, pass FROM profiles where email = '" . $email . "'";
+    //$stmt = $pdo->prepare($sql);
+    //$stmt->bindValue(':email', $email);
+    $conn->exec($sql);
+    
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    if($user === false) {
+        die('Incorrect username/password combination');
+        header('Location: index.php'); 
+    } else {
+        $validPassword = password_verify($passwordAttempt, $user['pass']);
+        if($validPassword) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['logged_in'] = time();
+            
+            header("Location: /home.php');
+            exit;
+        } else {
+            die('Incorrect username/password combination');
+            header('Location: index.php');
+        }
+    }
+}
+*/
+?>
+        
+
+    
 <!DOCTYPE html>
 <html>
 <head>
