d0876422ff6bc963dd08fe953fc9964c220c1431
Mitchell Roe <mbroe@oakland.edu>
2018-03-24 15:00:38 -0400

    Removed the column deletion statements and refactored the alter statements

--- config/Migrations/20180320145459_RemoveAndMoficationToReports.php
+++ config/Migrations/20180320145459_RemoveAndMoficationToReports.php
@@ -12,221 +12,59 @@ class RemoveAndMoficationToReports extends AbstractMigration
      */
     public function change()
     {
-      // execute()
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `DateCreated` `DateCreated` DATETIME NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `MiddleName` `MiddleName` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `Alias` `Alias` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `MissingEhnicityOther` `MissingEhnicityOther` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `MissingEyeColorOther` `MissingEyeColorOther` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `MissingHairColorOther` `MissingHairColorOther` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `MarksTattoos` `MarksTattoos` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `HeightFeet` `HeightFeet` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `HeightInches` `HeightInches` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `SocialMediaAccount` `SocialMediaAccount` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `ReportMiscInfo` `ReportMiscInfo` TEXT NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `Photo` `Photo` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `SeenName` `SeenName` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `SeenStreet` `SeenStreet` VARCHAR(20) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `SeenCity` `SeenCity` VARCHAR(20) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `SeenNumber` `SeenNumber` VARCHAR(10) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `SeenState` `SeenState` VARCHAR(2) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `SeenZip` `SeenZip` VARCHAR(6) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `SeenWhen` `SeenWhen` DATE NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `SeenNotes` `SeenNotes` TINYTEXT NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `FamilyFirstName` `FamilyFirstName` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `FamilyLastName` `FamilyLastName` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `FamilyGender` `FamilyGender` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `Relation` `Relation` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `FamilyStreet` `FamilyStreet` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `FamilyCity` `FamilyCity` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `FamilyState` `FamilyState` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `FamilyZip` `FamilyZip` VARCHAR(5) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `FamilyEthnicity` `FamilyEthnicity` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `FamilyEthnicityOther` `FamilyEthnicityOther` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `FamilyEmail` `FamilyEmail` TEXT NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `FamilyMiddleName` `FamilyMiddleName` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `RelationOther` `RelationOther` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `HangoutName` `HangoutName` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `HangoutStreet` `HangoutStreet` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `HangoutNumber` `HangoutNumber` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `HangoutCity` `HangoutCity` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `HangoutState` `HangoutState` VARCHAR(2) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `FamilyPhone` `FamilyPhone` VARCHAR(11) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `HangoutZip` `HangoutZip` VARCHAR(5) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `HangoutMisc` `HangoutMisc` TEXT NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `WorkplaceName` `Workplacename` VARCHAR(256) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `WorkplaceStreet` `WorkplaceStreet` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `WorkplaceNumber` `WorkplaceNumber` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `WorkplaceCity` `WorkplaceCity` VARCHAR(256) NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `WorkplaceState` `WorkplaceState` VARCHAR(2) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `WorkplaceZip` `WorkplaceZip` VARCHAR(5) NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `WorkplaceStartDate` `WorkplaceStartDate` DATE NULL ');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `WorkplaceEndDate` `WorkplaceEndDate` DATE NULL');
-             $this->execute('ALTER TABLE `mims`.`reports`
-               CHANGE COLUMN `WorkplaceMisc` `WorkplaceMisc` TEXT NULL');
-
-
-
-
-//       ALTER TABLE `mims`.`reports`
-// CHANGE COLUMN `DateCreated` `DateCreated` DATETIME NULL ,
-// CHANGE COLUMN `MiddleName` `MiddleName` VARCHAR(256) NULL ,
-// CHANGE COLUMN `Alias` `Alias` VARCHAR(256) NULL ;
-// CHANGE COLUMN `MissingEhnicityOther` `MissingEhnicityOther` VARCHAR(256) NULL ;
-// CHANGE COLUMN `MissingEyeColorOther` `MissingEyeColorOther` VARCHAR(256) NULL ;
-// CHANGE COLUMN `MissingHairColorOther` `MissingHairColorOther` VARCHAR(256) NULL ;
-// CHANGE COLUMN `MarksTattoos` `MarksTattoos` VARCHAR(256) NULL ;
-// CHANGE COLUMN `HeightFeet` `HeightFeet` VARCHAR(256) NULL ;
-// CHANGE COLUMN `HeightInches` `HeightInches` VARCHAR(256) NULL ;
-// CHANGE COLUMN `SocialMediaAccount` `SocialMediaAccount` VARCHAR(256) NULL ;
-// CHANGE COLUMN `ReportMiscInfo` `ReportMiscInfo` TEXT NULL ;
-// CHANGE COLUMN `Photo` `Photo` VARCHAR(256) NULL ;
-// CHANGE COLUMN `SeenName` `SeenName` VARCHAR(256) NULL ;
-// CHANGE COLUMN `SeenStreet` `SeenStreet` VARCHAR(20) NULL ;
-// CHANGE COLUMN `SeenCity` `SeenCity` VARCHAR(20) NULL ;
-// CHANGE COLUMN `SeenNumber` `SeenNumber` VARCHAR(10) NULL ;
-// CHANGE COLUMN `SeenState` `SeenState` VARCHAR(2) NULL ;
-// CHANGE COLUMN `SeenZip` `SeenZip` VARCHAR(6) NULL ;
-// CHANGE COLUMN `SeenWhen` `SeenWhen` DATE NULL ;
-// CHANGE COLUMN `SeenNotes` `SeenNotes` TINYTEXT NULL ;
-// CHANGE COLUMN `FamilyFirstName` `FamilyFirstName` VARCHAR(256) NULL ;
-// CHANGE COLUMN `FamilyLastName` `FamilyLastName` VARCHAR(256) NULL ;
-// CHANGE COLUMN `FamilyGender` `FamilyGender` VARCHAR(256) NULL ;
-// CHANGE COLUMN `Relation` `Relation` VARCHAR(256) NULL ;
-// CHANGE COLUMN `FamilyStreet` `FamilyStreet` VARCHAR(256) NULL ;
-// CHANGE COLUMN `FamilyCity` `FamilyCity` VARCHAR(256) NULL ;
-// CHANGE COLUMN `FamilyState` `FamilyState` VARCHAR(256) NULL ;
-// CHANGE COLUMN `FamilyZip` `FamilyZip` VARCHAR(5) NULL ;
-// CHANGE COLUMN `FamilyEthnicity` `FamilyEthnicity` VARCHAR(256) NULL ;
-// CHANGE COLUMN `FamilyEthnicityOther` `FamilyEthnicityOther` VARCHAR(256) NULL ;
-// CHANGE COLUMN `FamilyEmail` `FamilyEmail` TEXT NULL ;
-// CHANGE COLUMN `FamilyMiddleName` `FamilyMiddleName` VARCHAR(256) NULL ;
-// CHANGE COLUMN `RelationOther` `RelationOther` VARCHAR(256) NULL ;
-// CHANGE COLUMN `HangoutName` `HangoutName` VARCHAR(256) NULL ;
-// CHANGE COLUMN `HangoutStreet` `HangoutStreet` VARCHAR(256) NULL ;
-// CHANGE COLUMN `HangoutNumber` `HangoutNumber` VARCHAR(256) NULL ;
-// CHANGE COLUMN `HangoutCity` `HangoutCity` VARCHAR(256) NULL ;
-// CHANGE COLUMN `HangoutState` `HangoutState` VARCHAR(2) NULL ;
-// CHANGE COLUMN `FamilyPhone` `FamilyPhone` VARCHAR(11) NULL ;
-// CHANGE COLUMN `HangoutZip` `HangoutZip` VARCHAR(5) NULL ;
-// CHANGE COLUMN `HangoutMisc` `HangoutMisc` TEXT NULL ;
-// CHANGE COLUMN `WorkplaceName` `Workplacename` VARCHAR(256) NULL ;
-// CHANGE COLUMN `WorkplaceStreet` `WorkplaceStreet` VARCHAR(256) NULL ;
-// CHANGE COLUMN `WorkplaceNumber` `WorkplaceNumber` VARCHAR(256) NULL ;
-// CHANGE COLUMN `WorkplaceCity` `WorkplaceCity` VARCHAR(256) NULL ;
-// CHANGE COLUMN `WorkplaceState` `WorkplaceState` VARCHAR(2) NULL ;
-// CHANGE COLUMN `WorkplaceZip` `WorkplaceZip` VARCHAR(5) NULL ;
-// CHANGE COLUMN `WorkplaceStartDate` `WorkplaceStartDate` DATE NULL ;
-// CHANGE COLUMN `WorkplaceEndDate` `WorkplaceEndDate` DATE NULL ;
-// CHANGE COLUMN `WorkplaceMisc` `WorkplaceMisc` TEXT NULL ;
-
-
-
-
-
-
-    }
-    public function up()
-    {
-        $table = $this->table('reports');
-        $table->removeColumn('LastSeen');
-        $table->removeColumn('PlaceName');
-        $table->removeColumn('PlaceStreet');
-        $table->removeColumn('PlaceCity');
-        $table->removeColumn('PlaceState');
-        $table->removeColumn('PlaceZip');
-        $table->removeColumn('PlaceMiscInfo');
-        $table->removeColumn('Height');
-              $table->save();
-    }
-
-    public function down()
-    {
-        $table = $this->table('reports');
-        $table->addColumn('LastSeen', 'string', [
-                'default' => null,
-                'null' => false,
-        ]);
-        $table->addColumn('PlaceName', 'string', [
-                'default' => null,
-                'null' => false,
-        ]);
-        $table->addColumn('PlaceStreet', 'string', [
-                'default' => null,
-                'null' => false,
-        ]);
-        $table->addColumn('PlaceCity', 'string', [
-                'default' => null,
-                'null' => false,
-        ]);
-        $table->addColumn('PlaceState', 'string', [
-                'default' => null,
-                'null' => false,
-        ]);
-        $table->addColumn('PLaceZip', 'integer', [
-                'default' => null,
-                'limit' => 5,
-                'null' => false,
-        ]);
-        $table->addColumn('PlaceMiscInfo', 'string', [
-                'default' => null,
-                'null' => false,
-        ]);
-        $table->addColumn('Height', 'text', [
-                'default' => null,
-                'null' => false,
-        ]);
-
-
-              $table->update();
+        $reports = $this->table('reports');
+        $reports->changeColumn('DateCreated', 'datetime', ['null' => true])
+                ->changeColumn('MiddleName', 'text', ['null' => true])
+                ->changeColumn('Alias', 'text', ['null' => true])
+                ->changeColumn('MissingEthnicityOther', 'text', ['null' => true])
+                ->changeColumn('Alias', 'text', ['null' => true])
+                ->changeColumn('MissingEhnicityOther', 'text', ['null' => true])
+                ->changeColumn('MissingEyeColorOther', 'text', ['null' => true])
+                ->changeColumn('MissingHairColorOther', 'text', ['null' => true])
+                ->changeColumn('MarksTattoos', 'text', ['null' => true])
+                ->changeColumn('HeightFeet', 'text', ['null' => true])
+                ->changeColumn('HeightInches', 'text', ['null' => true])
+                ->changeColumn('SocialMediaAccount', 'text', ['null' => true])
+                ->changeColumn('ReportMiscInfo', 'text', ['null' => true])
+                ->changeColumn('Photo', 'text', ['null' => true])
+                ->changeColumn('SeenName', 'text', ['null' => true])
+                ->changeColumn('SeenStreet', 'text', ['null' => true])
+                ->changeColumn('SeenCity', 'text', ['null' => true])
+                ->changeColumn('SeenNumber', 'text', ['null' => true])
+                ->changeColumn('SeenState', 'text', ['null' => true])
+                ->changeColumn('SeenZip', 'text', ['null' => true])
+                ->changeColumn('SeenWhen', 'date', ['null' => true])
+                ->changeColumn('SeenNotes', 'string', ['null' => true])
+                ->changeColumn('FamilyFirstName', 'text', ['null' => true])
+                ->changeColumn('FamilyLastName', 'text', ['null' => true])
+                ->changeColumn('FamilyGender', 'text', ['null' => true])
+                ->changeColumn('Relation', 'text', ['null' => true])
+                ->changeColumn('FamilyStreet', 'text', ['null' => true])
+                ->changeColumn('FamilyCity', 'text', ['null' => true])
+                ->changeColumn('FamilyState', 'text', ['null' => true])
+                ->changeColumn('FamilyZip', 'text', ['null' => true])
+                ->changeColumn('FamilyEthnicity', 'text', ['null' => true])
+                ->changeColumn('FamilyEthnicityOther', 'text', ['null' => true])
+                ->changeColumn('FamilyEmail', 'text', ['null' => true])
+                ->changeColumn('FamilyMiddleName', 'text', ['null' => true])
+                ->changeColumn('RelationOther', 'text', ['null' => true])
+                ->changeColumn('HangoutName', 'text', ['null' => true])
+                ->changeColumn('HangoutStreet', 'text', ['null' => true])
+                ->changeColumn('HangoutNumber', 'text', ['null' => true])
+                ->changeColumn('HangoutCity', 'text', ['null' => true])
+                ->changeColumn('HangoutState', 'text', ['null' => true])
+                ->changeColumn('FamilyPhone', 'text', ['null' => true])
+                ->changeColumn('HangoutZip', 'text', ['null' => true])
+                ->changeColumn('HangoutMisc', 'text', ['null' => true])
+                ->changeColumn('WorkplaceName', 'text', ['null' => true])
+                ->changeColumn('WorkplaceStreet', 'text', ['null' => true])
+                ->changeColumn('WorkplaceNumber', 'text', ['null' => true])
+                ->changeColumn('WorkplaceCity', 'text', ['null' => true])
+                ->changeColumn('WorkplaceState', 'text', ['null' => true])
+                ->changeColumn('WorkplaceZip', 'text', ['null' => true])
+                ->changeColumn('WorkplaceStartDate', 'date', ['null' => true])
+                ->changeColumn('WorkplaceEndDate', 'date', ['null' => true])
+                ->changeColumn('WorkplaceMisc', 'text', ['null' => true])
+                ->save();
     }
 }
