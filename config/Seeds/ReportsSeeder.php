347476be70454fea9fa9fe0aa88a4e309b060ce2
Mitchell Roe <mbroe@oakland.edu>
2018-04-04 16:10:51 -0400

    Revert "Made the format a little easier"
    This reverts commit b9917ec5df4c797caaf3b8935d80e1b464978dea.
    
    Nothing outside of the `$data[$i] = [ ... ]` section was being stored.

--- config/Seeds/ReportsSeeder.php
+++ config/Seeds/ReportsSeeder.php
@@ -112,13 +112,10 @@ class ReportsSeeder extends AbstractSeed
             $gender = strtolower($data[$i]['Gender']);
             if ($gender == 'androgynous') $gender = null;
 
-            $data[$i] = [
-                'FirstName'  => $faker->firstName($gender),
-                'MiddleName' => $faker->randomElement([$faker->firstName($gender), null]),
-                'LastName'   => $faker->lastName(),
-                'Alias'      => $faker->randomElement([$faker->firstName($gender), null, null, null, null, null, null, null]),
-                /* 'DoB'        => $faker->randomElement([$faker->dateTimeBetween($startDate = '-70 years', $endDate = '-1 year'), null], null), */
-            ];
+            $data[$i]['FirstName']  = $faker->firstName($gender);
+            $data[$i]['MiddleName'] = $faker->randomElement([$faker->firstName($gender), null]);
+            $data[$i]['LastName']   = $faker->lastName();
+            $data[$i]['Alias']      = $faker->randomElement([$faker->firstName($gender), null, null, null, null, null, null, null]);
         }
         print_r($data);
     }
