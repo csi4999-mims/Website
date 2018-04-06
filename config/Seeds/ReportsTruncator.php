6d0ee47ccdaa94fe1aa34017e0deff9c2b7b1633
Mitchell Roe <mbroe@oakland.edu>
2018-04-06 19:53:47 -0400

    Formatting

--- config/Seeds/ReportsTruncator.php
+++ config/Seeds/ReportsTruncator.php
@@ -7,7 +7,7 @@ class ReportsTruncator extends AbstractSeed
     public function run()
     {
         /* If you're deleting all the reports, then there's no reason
-           to still have comments lying around. */
+           to still have the comments. */
         $comments = $this->table('comments');
         $comments->truncate();
 
