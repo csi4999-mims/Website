57956933217ae986b402a2786dbd02e6137106a2
Mitchell Roe <mbroe@oakland.edu>
2018-04-17 19:11:21 -0400

    Genderized the 'Other' relationships

--- config/Seeds/ReportsSeeder.php
+++ config/Seeds/ReportsSeeder.php
@@ -346,10 +346,19 @@ class ReportsSeeder extends AbstractSeed
 
             /* Relation Other (Friend/Family) */
             if ($data[$i]['Relation'] == 'Other') {
-                $data[$i]['RelationOther'] = $faker->randomElement([
-                    'Wife', 'Husband', 'Girlfriend', 'Boyfriend',
-                    'Spouse', null, null
-                ]);
+                if ($data[$i]['Gender'] == 'Female') {
+                    $data[$i]['RelationOther'] = $faker->randomElement([
+                        'Wife', 'Girlfriend', 'Spouse', null, null
+                    ]);
+                } elseif ($data[$i]['Gender'] == 'Male') {
+                    $data[$i]['RelationOther'] = $faker->randomElement([
+                        'Husband', 'Boyfriend', 'Spouse', null, null
+                    ]);
+                } else {
+                    $data[$i]['RelationOther'] = $faker->randomElement([
+                        'Spouse', 'Partner', null
+                    ]);
+                }
             }
 
             /* Family/Friend Location */
